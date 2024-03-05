a5f08af89a128acc16f3676cfbd2ac7b5cb1e7f0
bobbyhadz <opmftw@gmail.com>
2024-03-05 15:58:16 +0200

    minor change

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log('bobbyhadz.com');
+
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach(box => {
+  box.style.backgroundColor = 'purple';
+  box.style.width = '300px';
+});
